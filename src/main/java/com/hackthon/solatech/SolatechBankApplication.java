f2ff379e1402ba85c59d59dafb5ccd901aeb35dd
shanmukhrajiv <rajivvrns2@gmail.com>
2019-10-12 12:57:11 +0530

    Added project structure for solatech bank

---
+++ src/main/java/com/hackthon/solatech/SolatechBankApplication.java
@@ -0,0 +1,13 @@
+package com.hackthon.solatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolatechBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolatechBankApplication.class, args);
+	}
+
+}
